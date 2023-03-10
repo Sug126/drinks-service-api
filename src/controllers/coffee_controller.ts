d9237b7be4341f41344360213e381eb4ed28ccb5
Sugandha <raosugandha12@gmail.com>
2023-03-10 21:32:33 +0000

    implemented coffee endpoint

---
+++ src/controllers/coffee_controller.ts
@@ -0,0 +1,42 @@
+import request from "supertest";
+import { app } from "./app";
+
+describe("Test coffee API endpoint request", () => {
+  test("GET /coffeelover should return correct message", async () => {
+    const res = await request(app).get("/coffeelover");
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toEqual("I like coffee!");
+  });
+
+  test("GET /coffee should return correct object", async () => {
+    const res = await request(app)
+      .get("/coffee")
+      .query({ coffeeName: "Latte" });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      drinkType: "Coffee",
+      name: "Latte",
+    });
+  });
+
+  test("GET /coffee with no param should return correct object", async () => {
+    const res = await request(app).get("/coffee");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      drinkType: "Coffee",
+      name: "Latte",
+    });
+  });
+
+  test("GET /coffee with different param should return correct object", async () => {
+    const res = await request(app)
+      .get("/coffee")
+      .query({ coffeeName: "Frappuchino" });
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      drinkType: "Coffee",
+      name: "Frappuchino",
+    });
+  });
+});
